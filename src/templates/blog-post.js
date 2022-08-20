7b65b384a06d89ba4cd0371a0ea3704ab41cc124
caldwmark <caldwmark@gmail.com>
2022-08-19 20:57:11 -0800

    found more bio bits to remove

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { Link, graphql } from "gatsby"
 
-import Bio from "../components/bio"
+// import Bio from "../components/bio"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
@@ -27,9 +27,6 @@ const BlogPostTemplate = ({
           itemProp="articleBody"
         />
         <hr />
-        <footer>
-          <Bio />
-        </footer>
       </article>
       <nav className="blog-post-nav">
         <ul
